34b1a65c012657ee473dd72473962328e940689a
marquitosspa <159658490+marquitosspa@users.noreply.github.com>
2024-02-10 18:05:21 -0300

    primer comit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("goodBtn").addEventListener("click", function() {
+    window.location.href = "goodBtn.html";
+  });
+  
+  document.getElementById("badBtn").addEventListener("click", function() {
+    window.location.href = "badBtn.html";
+  });
+  
